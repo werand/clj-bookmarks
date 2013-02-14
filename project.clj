278019f0b1962c95ea807d6d17d514c62ca46e17
Andreas Werner <werner_andreas@web.de>
2013-02-14 20:24:19 +0100

    Updated depencies because of midje not working correctly.

--- project.clj
+++ project.clj
@@ -5,11 +5,10 @@ It provides both anonymous and named access. In the former case the APIs
 do not allow as many features as in the latter one."
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/data.zip "0.1.1"]
+                 [bultitude "0.1.7"] ;; <- dependency of midje
                  [clj-http "0.6.1"]]
   :min-lein-version "2.0.0"
-  :repositories {"stuart" "http://stuartsierra.com/maven2"}
   :profiles {:dev {:plugins [[marginalia "0.7.0"]
-                             [lein-midje "2.0.3"]
-                             [com.stuartsierra/lazytest "1.2.3"]]
+                             [lein-midje "2.0.4"]]
                    :dependencies [[midje "1.4.0"]]}}
   )
